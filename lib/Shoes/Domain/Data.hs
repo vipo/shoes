26ad87fc8ec967eac684efd6d777d59de10604d1
Vi Po <pi@homedir.eu>
2014-03-09 23:57:35 +0200

    add ixset

--- lib/Shoes/Domain/Data.hs
+++ lib/Shoes/Domain/Data.hs
@@ -11,26 +11,33 @@ import Data.Data(Data,Typeable)
 import Data.Acid(AcidState, Query, Update, makeAcidic, openLocalState)
 import Data.Acid.Advanced(query', update')
 import Data.SafeCopy(base, deriveSafeCopy)
+import Data.IxSet(Indexable(..), IxSet(..), (@=), Proxy(..), getOne, ixFun, ixSet, toList, toDescList)
 
 data ShoeData = ShoeData {
-  id :: String
+  shoeId :: String
   , description :: String
   , color :: String
   , size :: Integer
-  , photoFileName :: String	
-} deriving (Eq, Ord, Read, Show, Data, Typeable)
+  , photoFileName :: String
+} deriving (Eq, Ord, Show, Data, Typeable)
 $(deriveSafeCopy 0 'base ''ShoeData)
 
 data ShoeStorage = ShoeStorage {
   seq :: Int
-  , shoeSet :: [ShoeData]
-} deriving (Eq, Ord, Read, Show, Data, Typeable)
+  , shoeSet :: IxSet ShoeData
+} deriving (Eq, Ord, Show, Data, Typeable)
 $(deriveSafeCopy 0 'base ''ShoeStorage)
 
 fetchAll :: Query ShoeStorage [ShoeData]
-fetchAll = shoeSet <$> ask
+fetchAll = do
+  ShoeStorage{..} <- ask
+  return $ toDescList (Proxy :: Proxy String) $ shoeSet @= shoeId
 
 $(makeAcidic ''ShoeStorage ['fetchAll])
 
+instance Indexable ShoeData where
+  empty = ixSet
+    [ ixFun $ \bp -> [ shoeId bp ] ]
+
 initialDataState :: ShoeStorage
-initialDataState = ShoeStorage 0 []
+initialDataState = ShoeStorage 0 empty
